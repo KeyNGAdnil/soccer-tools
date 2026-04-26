16f6f856558a6d71e4ef4141765f25bb0fdf3cac
KeyNGAdnil <571047757@qq.com>
2026-04-26 22:22:13 +0800

    init

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,6 +4,9 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
+  server: {
+    base: '/soccer-tools/',
+  },
   html: {
     title: 'TIFO 工具箱',
     favicon: './public/favicon.svg',
